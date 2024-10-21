e4868ae6bce8aa05c9f739b154b4ce95d730e5d1
Eddy <Eddy@example.com>
2024-10-21 19:48:33 +1300

    banana fix

--- src/main/java/net/jeremy/mariomod/MariomodClient.java
+++ src/main/java/net/jeremy/mariomod/MariomodClient.java
@@ -1,11 +1,14 @@
 package net.jeremy.mariomod;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.jeremy.mariomod.entity.ModEntities;
+import net.minecraft.client.render.entity.FlyingItemEntityRenderer;
 
 public class MariomodClient implements ClientModInitializer{
     @Override
     public void onInitializeClient() {
-        
+        EntityRendererRegistry.register(ModEntities.BANANA_PROJECTILE, FlyingItemEntityRenderer::new);
     }
     
 }
